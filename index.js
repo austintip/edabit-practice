91e0356f4e4a3abc739d38c96498f3e3c46fe547
austintip <austin.d.tipograph@gmail.com>
2021-04-28 09:11:45 -0700

    make pair function

--- index.js
+++ index.js
@@ -46,3 +46,16 @@ function greeting(name) {
         return "Hello, " + name + "!";
     }
 }
+
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// makePair(512124, 215) ➞ [512124, 215]
+
+function makePair(num1, num2) {
+    return [...arguments]
+}
